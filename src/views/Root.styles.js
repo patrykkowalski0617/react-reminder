171e59e51a5cd92b12630657630f6b3847a4adc1
Patrycjusz Kowalski <patkowalski@neoidea.pl>
2022-03-11 10:16:03 +0100

    forms-and-routing-final

--- src/views/Root.styles.js
+++ src/views/Root.styles.js
@@ -1,10 +1,9 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  background-color: ${({ theme }) => theme.colors.white};
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
-  padding: 40px 30px;
-  border-radius: 25px;
-  margin: 20px auto;
-  box-shadow: 0 5px 15px -10px rgba(0, 0, 0, 0.3);
+  height: 100%;
 `;
